6c00eca252418ebdb66258edccf62d6af1ae82f1
Joseph Yhu <josephsyhu@gmail.com>
2020-11-19 18:20:25 -0500

    Update files.

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -18,7 +18,7 @@
         @foreach ($user->entries as $entry)
             <div class="col-xs-12 col-sm-12 col-md-12">
                 <div class="form-group">
-                    <a href="{{ route('entries.show', $entry->id) }}">{{ $entry->name }}</a>
+                    <a href="{{ route('entries.show', $entry->id) }}">{{ $entry->title }}</a>
                 </div>
             </div>
         @endforeach
