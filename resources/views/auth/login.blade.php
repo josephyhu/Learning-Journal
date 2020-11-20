5b6ae3b17aad792b86470ebc7ed698b8bf6c6356
Joseph Yhu <josephsyhu@gmail.com>
2020-11-19 20:25:34 -0500

    Added register link to the login page.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -34,10 +34,13 @@
 
             <div class="flex items-center justify-end mt-4">
                 @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
+                    <a class="underline text-sm text-gray-600 hover:text-gray-900 mr-2" href="{{ route('password.request') }}">
                         {{ __('Forgot your password?') }}
                     </a>
                 @endif
+                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('register') }}">
+                        {{ __('New User?') }}
+                    </a>
 
                 <x-jet-button class="ml-4">
                     {{ __('Login') }}
