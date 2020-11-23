f08b08439df081e82920e03ed75dbea3858300d8
Joseph Yhu <josephsyhu@gmail.com>
2020-11-22 23:51:50 -0500

    Update files.

--- resources/views/users/entries/show.blade.php
+++ resources/views/users/entries/show.blade.php
@@ -17,7 +17,7 @@
     <div class="row">
         <div class="col-xs-12 col-sm-12 col-md-12">
             <div class="form-group">
-                {{ $entry->date->format('F d, y')  }} {{ $entry->time->format('H:i') }}
+                {{ $entry->date->format('F d, Y')  }} {{ $entry->time->format('H:i') }}
             </div>
         </div>
         <div class="col-xs-12 col-sm-12 col-md-12">
